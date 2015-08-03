595adcaee2fc7e00bc56afb799c51086aadb2abd
Kujtim Shala <kshala@ford.com>
2015-08-03 16:12:06 +0200

    Manage lock screen requirements.

--- MobileWeather/AppDelegate.m
+++ MobileWeather/AppDelegate.m
@@ -11,11 +11,52 @@
 #import "LocationService.h"
 #import "SmartDeviceLinkService.h"
 
+@interface AppDelegate ()
+@property UIViewController *mainViewController;
+@property UIViewController *lockScreenViewController;
+@end
+
 @implementation AppDelegate
+
+- (void)lockScreen:(NSNotification *)notification {
+    @synchronized (self) {
+        UIViewController *root = [[self window] rootViewController];
+        // Display the lock screen if it is not presented already.
+        if ([root isEqual:[self lockScreenViewController]] == NO) {
+            [[self window] setRootViewController:[self lockScreenViewController]];
+        }
+    }
+}
+
+- (void)unlockScreen:(NSNotification *)notification {
+    @synchronized (self) {
+        UIViewController *root = [[self window] rootViewController];
+        // Display the regular screen if it is not presented already.
+        if ([root isEqual:[self mainViewController]] == NO) {
+            [[self window] setRootViewController:[self mainViewController]];
+        }
+    }
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Register every setting of the settings bundle and set it to its default value if it does not have any value set.
     [[NSUserDefaults standardUserDefaults] registerDefaultsFromSettingsBundle];
+    
+    [self setMainViewController:[[self window] rootViewController]];
+    [self setLockScreenViewController:[[UIViewController alloc]
+                                       initWithNibName:@"LockScreen" bundle:[NSBundle mainBundle]]];
+    
+    // Add observer to lock screen notifications
+    NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+    [center addObserver:self
+               selector:@selector(lockScreen:)
+                   name:SDLRequestsLockScreenNotification
+                 object:nil];
+    
+    [center addObserver:self
+               selector:@selector(unlockScreen:)
+                   name:SDLRequestsUnlockScreenNotification
+                 object:nil];
     
     [[SmartDeviceLinkService sharedService] start];
     
